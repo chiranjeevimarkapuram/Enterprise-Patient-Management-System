a0a6f9e73501f39b6c15f4fd12fce39c40117ff8
chiranjeevimarkapuram <chiranjeevimarkapuram16@gmail.com>
2026-05-09 23:39:10 +0530

    Initial commit

---
+++ src/main/java/com/CareSync/Enterprise/Patient/Management/System/EnterprisePatientManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CareSync.Enterprise.Patient.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisePatientManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterprisePatientManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CareSync/Enterprise/Patient/Management/System/EnterprisePatientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CareSync.Enterprise.Patient.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterprisePatientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
